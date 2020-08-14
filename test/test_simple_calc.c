f03323a933084e058f6e8cce82b92f16ea9f2401
stepin104923 <69148040+stepin104923@users.noreply.github.com>
2020-08-14 15:48:48 +0530

    Fix Indentation

--- test/test_simple_calc.c
+++ test/test_simple_calc.c
@@ -8,25 +8,25 @@ void tearDown(){}
 
 void test_simple_calc_add(void)
 {
-  TEST_ASSERT_EQUAL(15, simple_calc(CALC_OP_ADD, 10, 5));
-  TEST_ASSERT_EQUAL(15, simple_calc(CALC_OP_ADD, 0, 15));
+	TEST_ASSERT_EQUAL(15, simple_calc(CALC_OP_ADD, 10, 5));
+	TEST_ASSERT_EQUAL(15, simple_calc(CALC_OP_ADD, 0, 15));
 }
 
 void test_simple_calc_sub(void)
 {
-  TEST_ASSERT_EQUAL(5, simple_calc(CALC_OP_SUB, 10, 5));
-  TEST_ASSERT_EQUAL(-15, simple_calc(CALC_OP_SUB, 0, 15));
+	TEST_ASSERT_EQUAL(5, simple_calc(CALC_OP_SUB, 10, 5));
+	TEST_ASSERT_EQUAL(-15, simple_calc(CALC_OP_SUB, 0, 15));
 }
 
 int test_main(void)
 {
 /* Initiate the Unity Test Framework */
-  UNITY_BEGIN();
+	UNITY_BEGIN();
 
 /* Run Test functions */
-  RUN_TEST(test_simple_calc_add);
-  RUN_TEST(test_simple_calc_sub);
+	RUN_TEST(test_simple_calc_add);
+	RUN_TEST(test_simple_calc_sub);
 
-  /* Close the Unity Test Framework */
-  return UNITY_END();
+	/* Close the Unity Test Framework */
+	return UNITY_END();
 }
